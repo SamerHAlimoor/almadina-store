46914a8a52543c7ee309b3e3c3dca0a8970f3968
Samer Alimoor <sipar.alamour@gmail.com>
2022-11-28 11:01:38 +0200

    Live Tracking  finished private pusher

--- app/Http/Controllers/Front/OrdersController.php
+++ app/Http/Controllers/Front/OrdersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class OrdersController extends Controller
@@ -12,18 +13,23 @@ class OrdersController extends Controller
     //
     public function show(Order $order)
     {
-        $delivery = $order->delivery()->select([
-            'id',
-            'order_id',
-            'status',
-            DB::raw("ST_Y(current_location) AS lat"),
-            DB::raw("ST_X(current_location) AS lng"),
-        ])->first();
+        $user = Auth::user()->id;
+        if ($order->user_id === $user) {
+            $delivery = $order->delivery()->select([
+                'id',
+                'order_id',
+                'status',
+                DB::raw("ST_Y(current_location) AS lat"),
+                DB::raw("ST_X(current_location) AS lng"),
+            ])->first();
 
-        // return $delivery;
-        return view('front.orders.show', [
-            'order' => $order,
-            'delivery' => $delivery,
-        ]);
+            // return $delivery;
+            return view('front.orders.show', [
+                'order' => $order,
+                'delivery' => $delivery,
+            ]);
+        } else {
+            return abort(404);
+        }
     }
 }
